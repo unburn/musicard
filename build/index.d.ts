54754a5912f68cd7b75383dc055d722e4bb94935
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2023-09-27 20:42:48 +0600

    Add interface, change the name

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,15 +1,29 @@
-export declare class musicCard {
-    constructor();
+type timeType = string;
 
-    public setName(name: string): this;
-    public setAuthor(author: string): this;
-    public setColor(color: string): this;
-    public setTheme(theme: "classic" | "dynamic"): this;
-    public setBrightness(brightness: number): this;
-    public setThumbnail(thumbnail: string): this;
-    public setProgress(progress: number): this;
-    public setStartTime(starttime: string): this;
-    public setEndTime(endtime: string): this;
+interface MusicCardOptions {
+  author: string;
+  name: string;
+  color?: string;
+  theme?: "classic" | "dynamic";
+  brightness?: string;
+  thumbnail: string;
+  progress?: number;
+  starttime?: timeType;
+  endtime?: timeType;
+}
 
-    public build(): Promise<Buffer>;
+export declare class MusicCard {
+  constructor(options: MusicCardOptions);
+
+  public setName(name: string): this;
+  public setAuthor(author: string): this;
+  public setColor(color: string): this;
+  public setTheme(theme: "classic" | "dynamic"): this;
+  public setBrightness(brightness: number): this;
+  public setThumbnail(thumbnail: string): this;
+  public setProgress(progress: number): this;
+  public setStartTime(starttime: string): this;
+  public setEndTime(endtime: string): this;
+
+  public build(): Promise<Buffer>;
 }
